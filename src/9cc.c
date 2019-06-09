6f2557d1f494edf8600987fd5a40f9e99cce3f05
YUJI takahashi <jit.y.31@gmail.com>
2019-06-10 00:12:59 +0900

    Add tokenize
    enable to use whitespace between two tokens

--- src/9cc.c
+++ src/9cc.c
@@ -1,5 +1,80 @@
+#include <ctype.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+enum {
+    TK_NUM = 256,
+    TK_EOF,
+};
+
+typedef struct {
+    int ty;
+    int val;
+    char *input;
+} Token;
+
+char *user_input;
+
+Token tokens[100];
+
+void error(char *fmt, ...) {
+    va_list ap;
+
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+
+    exit(1);
+}
+
+void error_at(char *loc, char *msg) {
+    int pos = loc - user_input;
+
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr, "^ %s\n", msg);
+
+    exit(1);
+}
+
+void tokenize() {
+    char *p = user_input;
+    int i = 0;
+
+    while (*p) {
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-') {
+            tokens[i].ty = *p;
+            tokens[i].input = p;
+
+            i++;
+            p++;
+
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            tokens[i].ty = TK_NUM;
+            tokens[i].input = p;
+            tokens[i].val = strtol(p, &p, 10);
+
+            i++;
+
+            continue;
+        }
+
+        error_at(p, "トークナイズできません");
+    }
+
+    tokens[i].ty = TK_EOF;
+    tokens[i].input = p;
+}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -7,29 +82,46 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    char *p = argv[1];
+    user_input = argv[1];
+    tokenize();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("    mov rax, %ld\n", strtol(p, &p, 10));
 
-    while (*p) {
-        if (*p == '+') {
-            p++;
-            printf("    add rax, %ld\n", strtol(p, &p, 10));
+    if (tokens[0].ty != TK_NUM)
+        error_at(tokens[0].input, "数ではありません");
+
+    printf("    mov rax, %d\n", tokens[0].val);
+
+    int i = 1;
+    while (tokens[i].ty != TK_EOF) {
+        if (tokens[i].ty == '+') {
+            i++;
+
+            if (tokens[i].ty != TK_NUM)
+                error_at(tokens[i].input, "数ではありません");
+            printf("    add rax, %d\n", tokens[i].val);
+
+            i++;
+
             continue;
         }
 
-        if (*p == '-') {
-            p++;
-            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+        if (tokens[i].ty == '-') {
+            i++;
+
+            if (tokens[i].ty != TK_NUM)
+                error_at(tokens[i].input, "数ではありません");
+
+            printf("    sub rax, %d\n", tokens[i].val);
+
+            i++;
+
             continue;
         }
 
-        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-
-        return 1;
+        error_at(tokens[i].input, "予期しないトークンです");
     }
 
     printf("    ret\n");
--- test.sh
+++ test.sh
@@ -18,5 +18,6 @@ try() {
 
 try 0 0
 try 42 42
+try 41 " 12 + 34 - 5   "
 
 echo "OK"
